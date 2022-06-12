099a1a39b8b42b48780dd876dc83e13421a139a4
Sergey Krikun <sergey.a.krikun@gmail.com>
2022-06-12 16:43:24 +0300

    feat: background-sync

--- quesar-simplegram/src-pwa/custom-service-worker.js
+++ quesar-simplegram/src-pwa/custom-service-worker.js
@@ -15,8 +15,8 @@
   import {ExpirationPlugin} from 'workbox-expiration';
   import {CacheableResponsePlugin} from 'workbox-cacheable-response';
   import {NetworkFirst} from 'workbox-strategies';
-import {plugins} from "app/.postcssrc";
-
+  import {plugins} from "app/.postcssrc";
+  import {Queue} from 'workbox-background-sync';
 
 /*
   config
@@ -24,6 +24,19 @@ import {plugins} from "app/.postcssrc";
 
   precacheAndRoute(self.__WB_MANIFEST)
 
+  let backgroundSyncSupport = 'sync' in self.registration
+
+  // eslint-disable-next-line no-console
+  console.log('backgroundSyncSupport =>', backgroundSyncSupport);
+
+/*
+  queue - createPost
+*/
+
+  let createPostQueue = null;
+  if (backgroundSyncSupport) {
+    createPostQueue = new Queue('createPostQueue');
+  }
 
 /*
   caching strategies
@@ -59,4 +72,39 @@ import {plugins} from "app/.postcssrc";
       }
     )
   );
+
+/*
+  events - fetch
+ */
+
+// eslint-disable-next-line no-console
+console.log('backgroundSyncSupport =>', backgroundSyncSupport);
+
+if (backgroundSyncSupport) {
+  self.addEventListener('fetch', (event) => {
+    // Add in your own criteria here to return early if this
+    // isn't a request that should use background sync.
+
+    // eslint-disable-next-line no-console
+    console.log('event =>', event);
+    if (event.request.method !== 'POST') {
+      return;
+    }
+
+    const bgSyncLogic = async () => {
+      try {
+        const response = await fetch(event.request.clone());
+        return response;
+      } catch (error) {
+        await createPostQueue.pushRequest({request: event.request});
+        return error;
+      }
+    };
+
+    event.respondWith(bgSyncLogic());
+  });
+}
+
+
+
 
