d5a45d01df973defb6cb1208391aaf734fa714b7
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-01-28 23:58:01 +0300

    feat: add server fastify

--- graphql-typescript-helix/src/index.ts
+++ graphql-typescript-helix/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello")
+import "graphql-import-node";
+import fastify from "fastify";
+
+const PORT = 3001;
+
+async function main() {
+    const server = fastify();
+
+    server.get("/", (req, reply) => {
+        reply.send({ test: true });
+    });
+
+    server.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server is running on http://localhost:${PORT}/`);
+    });
+}
+
+main();
