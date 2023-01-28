ec70d024ce41303232f607b7140801126a4c82c8
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-01-29 00:21:17 +0300

    feat: add graphql-helix and GraphiQL

--- graphql-typescript-helix/src/index.ts
+++ graphql-typescript-helix/src/index.ts
@@ -1,13 +1,47 @@
 import "graphql-import-node";
 import fastify from "fastify";
+import { getGraphQLParameters, processRequest, Request, renderGraphiQL, shouldRenderGraphiQL, sendResult } from "graphql-helix";
+import { schema } from "./schema";
 
 const PORT = 3001;
 
 async function main() {
     const server = fastify();
 
-    server.get("/", (req, reply) => {
-        reply.send({ test: true });
+    server.route({
+        method: ["POST", "GET"],
+        url: "/graphql",
+        handler: async (req, reply) => {
+            const request: Request = {
+                headers: req.headers,
+                method: req.method,
+                query: req.query,
+                body: req.body,
+            };
+
+            if (shouldRenderGraphiQL(request)) {
+                reply.header("Content-Type", "text/html");
+                reply.send(
+                    renderGraphiQL({
+                        endpoint: "/graphql",
+                    })
+                );
+
+                return;
+            }
+
+            const { operationName, query, variables } = getGraphQLParameters(request);
+
+            const result = await processRequest({
+                request,
+                schema,
+                operationName,
+                query,
+                variables,
+            });
+
+            sendResult(result, reply.raw);
+        }
     });
 
     server.listen(PORT, "0.0.0.0", () => {
